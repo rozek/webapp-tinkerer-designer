04070e9481b19ad6bb25e1a6c4e91d778f63bb23
Andreas Rozek <A.Rozek@gmx.de>
2021-07-10 04:30:03 +0200

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -6,28 +6,36 @@ import resolve        from '@rollup/plugin-node-resolve'
 import autoPreprocess from 'svelte-preprocess'
 import typescript     from '@rollup/plugin-typescript';
 import postcss        from 'rollup-plugin-postcss'
-//import { terser }     from 'rollup-plugin-terser'
+import { terser }     from 'rollup-plugin-terser'
 
 export default {
   input: './src/index.ts',
   external:[                                 // list of (unbundled) dependencies
     'webapp-tinkerer-runtime',               // partial bundling
   ],
-  output: {
+  output: [{
     file:      './dist/webapp-tinkerer-designer.js',
     format:    'iife',
     name:      'WAD',
     globals:   { 'webapp-tinkerer-runtime':'WAT' },
     noConflict:true,
     sourcemap: true,
-    exports: 'default',
-  },
+    exports:   'default',
+  },{
+    file:      './dist/webapp-tinkerer-designer.min.js',
+    format:    'iife',
+    name:      'WAD',
+    globals:   { 'webapp-tinkerer-runtime':'WAT' },
+    noConflict:true,
+    sourcemap: true,
+    exports:   'default',
+    plugins:   [ terser({ format:{ comments:false, safari10:true } }) ],
+  }],
   plugins: [
     svelte({ preprocess:[
       autoPreprocess({ aliases:[['ts','typescript']] }),
     ]}),
     resolve({ browser:true, dedupe:['svelte'] }), commonjs(), typescript(),
     postcss({ extract:false, inject:{insertAt:'top'} }),
-//    terser({ format:{ comments:false, safari10:true } })
   ],
 };
