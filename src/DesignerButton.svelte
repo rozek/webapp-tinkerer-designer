0e695c27a35eec6cab7887f73979c68864a95926
Andreas Rozek <A.Rozek@gmx.de>
2021-07-18 07:49:54 +0200

    Update DesignerButton.svelte

--- src/DesignerButton.svelte
+++ src/DesignerButton.svelte
@@ -2,14 +2,16 @@
   .WAD-DesignerButton {
     display:block; position:absolute;
     width:32px; height:32px;
-    background:url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABA0lEQVRYR82WSw6EMAxD4bo9UK/LiAWjTCYfO6lUWEKwX01ocx6br3Oz/0EDzDmvCHqMQWlCxZmpB4TApADaPBNl610AVkingL5vAsiXsxVnTZxphQBd8wfugbD0/gCi4my10lCbebo/AFlcCICn4d03AarRW7+r1LJSWAYgxb24Q4DOt0fMvYb8JlAFYMxvCO3TAmDNlwJUzJcBVM2XAHTM2wBd8xbACvMQwHqo/125FVd2y/JOiJ7t2VkBA9xC1h6u72eG8jl0GEWfgTGzaqHjGBkgKiDUQCJT6ERuNTA0EXmdz3Y92rjvHcu9DmZ6AEktTUAbWmNXZ4OiAZgEkNrtAB9tuDAwYD8R4wAAAABJRU5ErkJggg==");
     cursor:pointer; pointer-events:auto;
   }
 </style>
 
 <script context="module" lang="ts">
   import      {       asDraggable        } from 'svelte-drag-and-drop-actions'
+  import              IconButton           from './IconButton.svelte'
   import type { WAT_Applet, WAT_Position } from 'webapp-tinkerer-runtime'
+
+  let ImageURL = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABA0lEQVRYR82WSw6EMAxD4bo9UK/LiAWjTCYfO6lUWEKwX01ocx6br3Oz/0EDzDmvCHqMQWlCxZmpB4TApADaPBNl610AVkingL5vAsiXsxVnTZxphQBd8wfugbD0/gCi4my10lCbebo/AFlcCICn4d03AarRW7+r1LJSWAYgxb24Q4DOt0fMvYb8JlAFYMxvCO3TAmDNlwJUzJcBVM2XAHTM2wBd8xbACvMQwHqo/125FVd2y/JOiJ7t2VkBA9xC1h6u72eG8jl0GEWfgTGzaqHjGBkgKiDUQCJT6ERuNTA0EXmdz3Y92rjvHcu9DmZ6AEktTUAbWmNXZ4OiAZgEkNrtAB9tuDAwYD8R4wAAAABJRU5ErkJggg=='
 
   let ButtonOffset = new WeakMap<WAT_Applet,WAT_Position>()// remember positions
 </script>
@@ -32,4 +34,7 @@
   left:{Applet.x + Offset.x}px; top:{Applet.y + Offset.y}px
 " use:asDraggable={{ onDragStart, onDragMove }}
   on:click={onClick}
-></div>
+>
+  <IconButton {ImageURL}></IconButton>
+</div>
+
