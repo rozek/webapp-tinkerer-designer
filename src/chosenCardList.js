19a00fc8d80b394549ba68aef4c559c002a15c2b
Andreas Rozek <A.Rozek@gmx.de>
2021-07-19 17:32:42 +0200

    Update chosenCardList.js

--- src/chosenCardList.js
+++ src/chosenCardList.js
@@ -1,2 +1,43 @@
   import { writable } from 'svelte/store'
-  export const chosenCardList = writable([])
+
+  import { ValuesDiffer } from 'webapp-tinkerer-runtime'
+  import { chosenApplet } from './chosenApplet.js'
+
+  let currentlyChosenApplet = undefined
+  let currentCardList       = []
+
+  const chosenCardListStore = writable([])        // for subscription management
+
+/**** keep track of changes in "chosenApplet" ****/
+
+  chosenApplet.subscribe((newChosenApplet) => {  // implements a "derived" store
+    if (currentlyChosenApplet !== newChosenApplet) {
+      currentlyChosenApplet = newChosenApplet
+
+      let newCardList = (newChosenApplet == null ? [] : newChosenApplet.CardList)
+      if (ValuesDiffer(currentCardList,newCardList)) {
+        currentCardList = newCardList
+        chosenCardListStore.set(currentCardList)
+      }
+    }
+  })
+
+/**** keep track of all cards in "chosenApplet" ****/
+
+  function updateChosenCardList () {
+    if (currentlyChosenApplet != null) {
+      let newCardList = currentlyChosenApplet.CardList
+      if (ValuesDiffer(currentCardList,newCardList)) {
+        currentCardList = newCardList
+        chosenCardListStore.set(currentCardList)
+      }
+    }
+  }
+
+  setInterval(updateChosenCardList, 300)
+
+/**** export an explicitly implemented store ****/
+
+  export const chosenCardList = {
+    subscribe: (Subscription) => chosenCardListStore.subscribe(Subscription)
+  }
