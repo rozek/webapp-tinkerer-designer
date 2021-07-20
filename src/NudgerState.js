6ef4fe02df0c7ea4f386dec600fd1b44f437657f
Andreas Rozek <A.Rozek@gmx.de>
2021-07-20 08:55:09 +0200

    Create NudgerState.js

---
+++ src/NudgerState.js
@@ -0,0 +1,53 @@
+  import { writable } from 'svelte/store'
+
+  import { ValuesDiffer } from 'webapp-tinkerer-runtime'
+  import { chosenApplet } from './chosenApplet.js'
+
+  const initialNudgerState = {
+    isVisible:false, Offset:{ x:NaN,y:NaN }, Width:NaN,Height:NaN
+  }
+
+  let currentlyChosenApplet = undefined
+  let currentNudgerState   = Object.assign({}, initialNudgerState)
+
+  const NudgerStateStore = writable(currentNudgerState)   // subscription mgmt
+  const NudgerStateSet   = new WeakMap()      // applet-specific Nudger states
+
+/**** keep track of changes in "chosenApplet" ****/
+
+  chosenApplet.subscribe((newChosenApplet) => {  // implements a "derived" store
+    if (currentlyChosenApplet !== newChosenApplet) {
+      currentlyChosenApplet = newChosenApplet
+
+      if (currentlyChosenApplet == null) {
+        currentNudgerState = Object.assign({}, initialNudgerState)
+      } else {
+        if (NudgerStateSet.has(currentlyChosenApplet)) {
+          currentNudgerState = NudgerStateSet.get(currentlyChosenApplet)
+        } else {
+          currentNudgerState = Object.assign({}, initialNudgerState)
+          NudgerStateSet.set(currentlyChosenApplet,currentNudgerState)
+        }
+        NudgerStateStore.set(currentNudgerState)
+      }
+    }
+  })
+
+/**** validate changes to "NudgerState" ****/
+
+  function setNudgerState (newNudgerState) {
+    if (currentlyChosenApplet !== null) {
+      if (ValuesDiffer(currentNudgerState,newNudgerState)) {
+        currentNudgerState = Object.assign({}, newNudgerState)
+        NudgerStateSet.set(currentlyChosenApplet,newNudgerState)
+        NudgerStateStore.set(newNudgerState)
+      }
+    }
+  }
+
+/**** export an explicitly implemented store ****/
+
+  export const NudgerState = {
+    subscribe: (Callback) => NudgerStateStore.subscribe(Callback),
+    set:       setNudgerState
+  }
