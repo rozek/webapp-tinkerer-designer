423a76d41df4e673934d4f3c7bfdd685993977c1
Andreas Rozek <A.Rozek@gmx.de>
2021-07-20 14:40:59 +0200

    Update InspectorView.svelte

--- src/InspectorView.svelte
+++ src/InspectorView.svelte
@@ -17,7 +17,21 @@
 
 /**** normal IconButton images as Data URLs ****/
 
-  let ImageURL = ''
+  let AppletImageURL       = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAA8UlEQVRYR+1XQQ7DIAxrv8uD+G4nKqXKsqQhpoEetsMmsYXYxvHovi1+7Yv7b+8BUGs9ZqpRSjnJn2+tOS3MAkE90wBIRSXBNADUWGvY1KX1VAAWW37UKQA0L9Ga9fmoByQA2ZQbPl0Brfl0ANx0NN4S2KNH4GVKugkJgMXcHUPLRHJjLbatuSfpu4Io4mJPbi/S1SnwZlY2HfkPCQPgjGScemy1780cuAuPdAUQJmjNjwLoRiN17SivIBrZCK19DwAeNCibaN3XnTBarAFG75Twtdy78/WS+gOAFLAeYhAfhAD0Pj1FgIQA9Bor8rvlAD4fTngwzU/HXwAAAABJRU5ErkJggg=='
+  let MasterImageURL       = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABY0lEQVRYR82Xaw7CMAyD2XV3oF0XVKSg4NlOuoeAf7DSfnGdx5bHjz/L2fO3bXvGHuu6Tu83/YcMPA7Phx6BuRQA4TrKHAbA6Mdh7Lf4XcHcBuBg8rXtAOIelaHyPY+oYh3zA3uG674A8kNlMARjxsN9ApQF9wHAjfJiJafKCBUIg30DsD84NVjtiPXd6GNdCaDcnu8/gFBiBZODngZQJmRK5rsf3mGq3gqAWXAJQC4sLCPYoajE1BW4Cuf6AvPFIQXOAGBmtAEwzcJAHeerhoS9oEzDLKFquViK3XdVb3ZZ4AqTU4BF2OkVJQC2U1YFWat1zWmXBcxoqhl1ALqHj72+mpHL60qJTltmvpLt2JkwH5blryJnStOBBF1fKaNqRQeIjmRV/6+GEOYTtaecCR0Ea7MsRdU1ZkA7lCoINSOwObJS878BuuaaWYf+KN8LcAJiVc/ViOp9sQRwle+KZz8HeAHatLA/TmjFBQAAAABJRU5ErkJggg=='
+  let CardImageURL         = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABBElEQVRYR+2WQRLEIAgEk+/6IL+7Wx5IEQPMkBi9ZE/ZUqGZUXHfFv/2xfm3D4BWoNb6y9hVSqFiw0k6MRu0rWHnugCSWAKhoHo+mquVvAD0iWUyCirj3nrPvgMALYwArDEUT4BOAJFvWQCtXPv2Yp8suJMEWYNApgF4IOYmtOTyfGaPm96EOtZ0gL4Q8x5gqmW974/fUgALOrwJtb968ajqmzrTADzosBlZVY+sPlSgDY4CiKCpdtz2Anu3W01nKQCyDCogVkQNJXopLQVAyeEm7O9v/Z/pAa8A6CdaZAuTPKWA5/PdJ9zlRZR5cntHTRRhqx+igNXtMieGOoZP1YnWfwB/a0o8MCIQpw0AAAAASUVORK5CYII='
+  let OverlayImageURL      = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAs0lEQVRYR+2W0Q6AIAhF9Xf9IH+35gObI/Qi0qyNXmoT5HZAIafDTz4cP4UAkUCt9fJITSkFEn4YUPDm3L75e1UYEjEUsBpoZG8SgJy04ojgzF4k8FsBmuLlP+dK4BMCZumTasKdAB1bXnj9ce7XXhEwqvogEASCQBBQEWiXiOZOn/V/yV99E2p7vWSH+r+agFVEP8KZe4E1OKVuuxvuCkD+cCBBG3ivw7ndOyDfLwQcJ3AD7On8Ic5p3nwAAAAASUVORK5CYII='
+  let ComponentImageURL    = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABAklEQVRYR+1WQRLEIAhrv+uD/O7ueGCGYQOh1FoP21OnFhMiBM/j5ed8Gf/Ym0Dv/WMVaq1B0ujfTKyrQLShJZEBFzI2lmajAzQQ+u6pM8AllhLwQCQDtK43z7zro/lRADEd36KMEagGGbGXFRBAljGSNxMjBPdVYDBERcWyu10DWlJLYkkXoGpnhjLdByISy5xw1ZB6bBh5fW8Te4SArYnIouksqB5FZGTUiiPGjJCV3voCHUbWBxigt15WoAqo427XwEwS7DhpF2TbqUo6JHBFyqkEoolXBfKmK7wPoBvMjKOwN6tBihoRaqeqCqgg96wB1NOsnaqq0DasbpyN+xP4ArhxGDBtdoDkAAAAAElFTkSuQmCC'
+  let ImportExportImageURL = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAA0klEQVRYR+2VYQ6AIAiF67oeyOvWbKPRExCdaW32p5qO94Hw3LfJzz5Zf1sA/61AjPEIIVwJpO/0pv+avmqqAAoOBSAxnvEwAC4+HADFhwJI4p8EwO73TEXXKeAAHvG0XwWQup0EtDGkdfQHyyNEAG4ykqlYAFrmWswMAB1OClhjRKV4D4BS5tYReM8cNW4Ayc28QJb3YwzUMQFqLhXv3iqAKRXAUfJmVtqn3SPZEZQC9Vqnps2moJeAFYdPjGpEb4BIo9p0F/SEWwCrAqsC0ytwArCqxCFYK1feAAAAAElFTkSuQmCC'
+  let SearchImageURL       = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAA00lEQVRYR+2WQRKAIAgA87s+yO/WcHCGEBAV8WKnmqxdkKD0HD7SYf7jLlBKeWlQOWeR4ybAgS0iLgIYzkWr3V8W6MFrFqR1bgLaPlMJvHZJoEZlgUsSV+BmYCkDUFi0EOFaKkquaN0EQKaCuW/etQ9obXd7J8Rp5M616crJDW0Bt9849bgmsIjLNJSKTSs6y7+GKQO74CDYFdgJ7wrshqsCEXBRIArOCkTCG4Fo+E/gBLwRwMMEzlebzFAjotMqAi7WAO3vlkhm13Q74eyLrc9dgQ9itQQwkRB2awAAAABJRU5ErkJggg=='
+
+  let ContentsImageURL               = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAUklEQVRYR+3UwREAIAgDQWiXgtKuFqA+MTzOAiSzBjPMJ83zgwAIXAUkrY5yVtUxjwAzBTre/3UnazizA/wDdgHW0C5g7wAB7AL2EhIAAQR+CmweoTAhD/IaqwAAAABJRU5ErkJggg=='
+  let SelectionGlobalsImageURL       = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAA+UlEQVRYR+2XUQ7EIAhE2+t6IK+7Gz5oKAEZ1IZusv3ZJlV5DgO651H8nMXxjwug9/5hmNZaCLZrvAtAAQhk9CuBs+N57nsUqPLCLdcs41Mw1vqh2SwYacCMca21UgpwYK4SuSP9zQPXFQYroOXTrueA2TRCCnjBvaAexJQHouBZCJ2aUIGdAGkFrAkZeRE/DDshunskDd7ZAQEgfX50bhDgEgBaYiPFQgC0cTzmAZZJdyrUB8i4dBVYUKgCSAXQ+mEfQCCQ3XsK/8ZZIKuA3ktOQ1klZfeB1ZvSVBWsBo3mv+dWrFsl0v///wui/CLf4UaELDYzphzgC1oI5DDpfBDzAAAAAElFTkSuQmCC'
+  let SelectionResourcesImageURL     = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAA7ElEQVRYR+2XUQ6EMAhE9bo9UK+7m35gugSYEWl0N+uPMVIYnhTqvt187TfH3w4BvfeXiGmtQWFV9q6AEWAIie6z4LP2sjZFYM5+OLpCDKJeXSMfAgTjqqCW/98hIHUR1cNSAowA69NSNYCc6/eefZpAlYAUgbPBJYi1LkWgUkBIwOrt2eAWBW92hK340QKQuEFhtoEEvPYbbaloEDECx3rYB7xqRlOwbBdolNazpsdmTxHwviXq+RahVB+4kh0z1mENME5YmxICbDDW7k8gdSqedwbqB3ouaPvv/C9YQoCt2mq7Zx3Lq7Nj/L0BoDBwMIhTLVUAAAAASUVORK5CYII='
+  let SelectionPropertiesImageURL    = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABM0lEQVRYR+1XQQ7DMAhrv9sH5bubcqDykMEQReoO3WWHJsUY20nP4+Hf+XD94wYwxvgYmOu6JLBd60MAs8AEkv0j4O562/s/DHS1YB2zfTaeyih/Zp29FAtFBVAXuN6AsPdLsfkOI/FFxZWw2wxUAGDHTKjYVJkBT7unkwGrWLXEgKfX7Dk7Yd0yYaKlWwyw2bJ8mC9VImTASgxEQmRMIBBvwy0uMDBR8nXzZDkJfacqfCJBlgBkaq4y0QaAHfnNzJLmiEpKojbCHFDJhha0cUSizHSRuoB1k826ogPvDMqASjD1HBlRrqAMqAJKlL5o+zRcBRCJ1esljOLogMG5dTXg3VTSQJbr7KKB6eiP3+X7QCcH2G1p641IaYIpnR086WnYLbJr/ftdUL4TqkRbff4C+AI8xegwDnQdAwAAAABJRU5ErkJggg=='
+  let SelectionConfigurationImageURL = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABGUlEQVRYR82WXQ4DIQiEd6/rgbxuGx9oKOFnQBLdl+42qJ/jAL7P4ec9vP7zA5hzfghmjBGCdcWbAGuBBeL9cuBsPI29R4FTXvg7a5KRYOQ3Ckn+kF7S5jPNljUZh17viJFXXKhAJjNWrKdiSoHqjrj81lHwo3QV4IGIH/ixacqVFEAhqp6BFfAyA118WwENAl3cSuGWSoiknQXaBhBBtALIyWQj0uQOAbJllsdrXROdL5UF2s659JESW1lgSSirXQQhlYF7gdcTUIiUAtmajkLAClTuBhqE5hGzGVnORbqa7J4yS8JKqPVy+i8qMnJyz7DyohJev9F8rsa1lGLk+hVWwqi8yvzejSfF7lGgeoa74+4x4e5OquO/i3SsMOeJ2OQAAAAASUVORK5CYII='
+  let SelectionScriptImageURL        = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABFElEQVRYR+1XUQ5CMQh777o70K6r4QODCKPAkmmiPyaGja4t3byvw5/7cP/rBWDO+WAwY4wQ2K56FwA1ICCrbwk4W89rv4eBU15405pptMBIzbNg2VPW/qHZqFmneWTskIGs27mhBi0NLRkMGeCNkNH0mtPv3nqYAamjHj95Ij6p1bTkAc2AlsSjenVqVwILYUcCTfsWBpARzIAOk9CTQGqt6bYAeNP0ewCqEoQMeBtn9NQ5gGRHKwdY+5VP5MG2TAEy9xnW0gxUPcCXmpbFvAt23H6tJESeV8hzrZyEUsMKGPT0VOd6oCODN36lKUBM16kJozgKKJTuMAmtex4xmgyjTP3/f8EHAx0jddaGr+LO5sjaJ0cgzDAHqlx2AAAAAElFTkSuQmCC'
+  let SelectionContentsImageURL      = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAqklEQVRYR+2X3QqAIAxG9XV9oL1u4YVh5JyOb2yQ3QSx9HD2I+bk/GTn/dMDQERXgymliGCoeBagblBBZu8eeDe+/RvHgFctvHLdNFrBjNYXi80K5lMD9UNIA3277dpYaWexBpAAqhpAAowMigZ2tc/iVQaQAFMD3GxHpYBbXxzF/wGwzjW3fvwuQNWA2gASQDUHkABnEh4DqrOAa59zL4AbCHEWeEC43wtuK9YEMALX8OsAAAAASUVORK5CYII='
 
 /**** keep track of every Applet's Inspector state ****/
 
@@ -45,8 +59,24 @@ $:if (Applet != null) {                                            // needs "$:"
   >
     <div name="TabStrip" style="
       display:block; position:relative;
-      height:74px; overflow:visible;
+      top:2px; height:74px; overflow:visible;
       border-bottom: solid 1px #454545;
-    "></div>
+    ">
+      <IconButton style="left:10px;  top:0px" ImageURL={AppletImageURL}/>
+      <IconButton style="left:50px;  top:0px" ImageURL={MasterImageURL}/>
+      <IconButton style="left:90px;  top:0px" ImageURL={CardImageURL}/>
+      <IconButton style="left:130px; top:0px" ImageURL={OverlayImageURL}/>
+      <IconButton style="left:170px; top:0px" ImageURL={ComponentImageURL}/>
+      <IconButton style="left:210px; top:0px" ImageURL={ImportExportImageURL}/>
+      <IconButton style="left:250px; top:0px" ImageURL={SearchImageURL}/>
+
+      <IconButton style="left:10px;  top:40px" ImageURL={ContentsImageURL}/>
+      <IconButton style="left:50px;  top:40px" ImageURL={SelectionGlobalsImageURL}/>
+      <IconButton style="left:90px;  top:40px" ImageURL={SelectionResourcesImageURL}/>
+      <IconButton style="left:130px; top:40px" ImageURL={SelectionPropertiesImageURL}/>
+      <IconButton style="left:170px; top:40px" ImageURL={SelectionConfigurationImageURL}/>
+      <IconButton style="left:210px; top:40px" ImageURL={SelectionScriptImageURL}/>
+      <IconButton style="left:250px; top:40px" ImageURL={SelectionContentsImageURL}/>
+    </div>
   </Dialog>
 {/if}
