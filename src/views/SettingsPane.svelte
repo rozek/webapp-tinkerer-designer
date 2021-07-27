e81bb29bec4022bd7daa4921e32c66094d22de7e
Andreas Rozek <A.Rozek@gmx.de>
2021-07-27 08:13:14 +0200

    Create SettingsPane.svelte

---
+++ src/views/SettingsPane.svelte
@@ -0,0 +1,16 @@
+<style>
+  .WAD-SettingsPane {
+    display:flex; position:absolute; flex-flow:column nowrap;
+    width:100%; height:100%;
+    padding:4px;
+  }
+</style>
+
+<script context="module" lang="ts">
+</script>
+
+<script lang="ts">
+</script>
+
+<div class="WAD-SettingsPane">
+</div>
